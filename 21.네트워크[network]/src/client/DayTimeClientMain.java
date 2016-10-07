6763962c19dd16db8d2bbddf64dd23741bf8b999
sumin <tn841@naver.com>
2016-10-07 15:12:14 +0900

    InputStreamReader를 이용하여 byte를 char로 변경(2바이트 씩 읽는다. 브릿지스트림) BufferedReader로 최종 필터링 하여 문자를 한 라인씩 읽는다.

--- 21.네트워크[network]/src/client/DayTimeClientMain.java
+++ 21.네트워크[network]/src/client/DayTimeClientMain.java
@@ -17,20 +17,20 @@ public class DayTimeClientMain {
 		 */
 		Socket socket = new Socket("192.168.12.31", 8000);
 		System.out.println("socket생성 : "+socket);
-		
+		System.out.println("내IP:"+socket.getLocalAddress());
 		/*
 		 * 2.소켓이 데이서의 소스이고 소스에 빨때를 꽂아서 데이터를 읽어드리자.
 		 */
 		InputStream in = socket.getInputStream();	//읽어드리는 빨대 생성 (1바이트씩 읽음 - 매우 불편)
-		//BufferedInputStream bis = new BufferedInputStream(in);	//버퍼드 스트림을 이용 (4바이트씩 한번에 읽음 - 편해짐)
-		InputStreamReader isr = new InputStreamReader(in,"EUC-KR");	//Reader를 이용하여 문자읽기..
-		BufferedReader socketReader = new BufferedReader(isr);
+		//BufferedInputStream bis = new BufferedInputStream(in);	//버퍼드 스트림을 이용 (4바이트씩 한번에 읽음, 필터스트림 - 편해짐)
+		InputStreamReader isr = new InputStreamReader(in,"EUC-KR");	//Reader를 이용하여 문자읽기..(브릿지 스트림)
+		BufferedReader socketReader = new BufferedReader(isr);		//문자를 한 라인씩 읽는다. (필터 스트림)
 	
-		//System.out.println(socketReader.readLine());
+		System.out.println(socketReader.readLine());
 	
+		 
 		
-		
-		while(true){
+	/*	while(true){
 			int readByte = in.read();
 			System.out.print((char)readByte+"");
 			if(readByte == -1){
@@ -38,7 +38,7 @@ public class DayTimeClientMain {
 				break;
 			}
 		}
-		
+		*/
 		
 		
 	}
