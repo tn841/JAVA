f5a03534af9c756071030c53d8af2cc31653e1d1
sumin <tn841@naver.com>
2016-10-07 14:10:01 +0900

    소켓을 이용하여 네이버의 열린 포트 찾기

--- 21.네트워크[network]/src/client/SocketPortScanMain.java
+++ 21.네트워크[network]/src/client/SocketPortScanMain.java
@@ -1,10 +1,22 @@
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class SocketPortScanMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		for(int port = 0; port< 65536; port++){
+			try{
+				Socket socket = new Socket("www.naver.com",port);	//도메인을 적으면 DNS에서 IP주소를 가져옴
+				System.out.println(port+"포트 소켓 생성");
+			}catch(Exception e){
+				
+			}
+		}
+		
 	}
 
 }
