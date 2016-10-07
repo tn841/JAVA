2c9d482a50902ac02ffb2b58a4e735fca4defcee
sumin <tn841@naver.com>
2016-10-07 11:57:22 +0900

    최초 업로드

---
+++ 21.네트워크[network]/src/client/InetAdreesMain.java
@@ -0,0 +1,12 @@
+package client;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAdreesMain {
+
+	public static void main(String[] args) throws UnknownHostException {
+		InetAddress.getByName("www.naver.com");
+	}
+
+}
