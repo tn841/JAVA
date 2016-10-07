f8e54efb99a603bc9d561b7a3a07e2c7b7c4ca9f
sumin <tn841@naver.com>
2016-10-07 14:17:27 +0900

    21번 포트 확인

--- 21.네트워크[network]/src/client/SocketPortScanMain.java
+++ 21.네트워크[network]/src/client/SocketPortScanMain.java
@@ -8,12 +8,12 @@ public class SocketPortScanMain {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		
-		for(int port = 0; port< 65536; port++){
+		for(int port = 20; port< 22; port++){
 			try{
-				Socket socket = new Socket("www.naver.com",port);	//도메인을 적으면 DNS에서 IP주소를 가져옴
+				Socket socket = new Socket("www.microsoft.com",port);	//도메인을 적으면 DNS에서 IP주소를 가져옴
 				System.out.println(port+"포트 소켓 생성");
 			}catch(Exception e){
-				
+				System.out.println(port+"포트 소켓 생성 실패");
 			}
 		}
 		
