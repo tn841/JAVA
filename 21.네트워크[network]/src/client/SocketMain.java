883935f83dcb44de3520178458a19e54be90072b
sumin <tn841@naver.com>
2016-10-07 14:08:15 +0900

    소켓을 이용하여 네이버의 열린 소켓 찾기

--- 21.네트워크[network]/src/client/SocketMain.java
+++ 21.네트워크[network]/src/client/SocketMain.java
@@ -1,13 +1,25 @@
 package client;
 
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class SocketMain {
 
 	public static void main(String[] args) throws Exception{
-		Socket socket=new Socket("125.209.222.142", 80);
+		Socket socket=new Socket("125.209.222.142", 80);	//네이버와 소켓 연결, 소켓은 아래의 4개 정보를 가진다.
+															//네이버 서버는 80포트로 오는 소켓을 받을 준비를 하고 있기 때문에 연결이 되는 것.
 		System.out.println("connect:"+socket);
-
+		
+		InetAddress remoteAddr = socket.getInetAddress();	//네이버 측 IP
+		int remotePort = socket.getPort();					//네이버 측 Port
+		InetAddress localAddr = socket.getLocalAddress();	//내 로컬 IP
+		int localPort = socket.getLocalPort();;				//내 로컬 Port
+		
+		System.out.println("네이버 : "+remoteAddr + " - "+ remotePort);
+		System.out.println("로컬 : "+localAddr + " - "+ localPort);
+		
+		socket.close();//소켓 닫기
+		
 	}
 
 }
---
+++ 21.네트워크[network]/src/client/SocketPortScanMain.java
@@ -0,0 +1,10 @@
+package client;
+
+public class SocketPortScanMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
